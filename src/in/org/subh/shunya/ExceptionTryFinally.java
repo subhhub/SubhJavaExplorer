31cc7c0f1b7a2d4aecd948802cc1a9936397f9bd
subhash kewat <subhash.kewat@ericsson.com>
2017-08-22 18:09:44 +0530

    change2

--- src/in/org/subh/shunya/ExceptionTryFinally.java
+++ src/in/org/subh/shunya/ExceptionTryFinally.java
@@ -5,7 +5,10 @@ public class ExceptionTryFinally {
 	public static void main(String[] args) {
 		System.out.println("Exception Try Finally Main");
 		
-		int rs = new ExceptionTryFinally().doTheWork();
+//		int rs = new ExceptionTryFinally().doTheWork();
+//		System.out.println(rs);
+
+		int rs = new ExceptionTryFinally().doTheWork7();
 		System.out.println(rs);
 		
 	}
@@ -13,24 +16,47 @@ public class ExceptionTryFinally {
 	public int doTheWork() {
 		try {
 			System.out.println("Before Exception ");
-			int vl = 10/0;
-			System.out.println("After Exception ");
-			return 2;
+//			int vl = 10/0;
+			throw new NullPointerException();
+//			System.out.println("After Exception ");
+//			return 2;
 		} catch (ArithmeticException e) {
 			System.out.println("Exception "+e);
-//			return 0;
+			return 3;
 		} catch (NullPointerException e) {
 			System.out.println("Exception "+e);
-//			return 0;
-		}/*finally{
+			return 4;
+		}finally{
 			System.out.println("All done");
 			try {
 				int vl = 10/0;
 			} catch (Exception e) {
 				System.out.println("hello math");
 			}
-			return 1;
-		}*/
-		return 1;
+			return 5;
+		}
+//		return 1;
+	}
+	
+	public int doTheWork7() {
+		try {
+			System.out.println("Before Exception ");
+//			int vl = 10/0;
+			throw new NullPointerException();
+//			System.out.println("After Exception ");
+//			return 2;
+		} /*catch (ArithmeticException | NullPointerException e) {
+			System.out.println("Exception "+e);
+			return 3;
+		}*/ finally{
+			System.out.println("All done");
+			try {
+				int vl = 10/0;
+			} catch (Exception e) {
+				System.out.println("hello math");
+			}
+			return 5;
+		}
+//		return 1;
 	}
 }
