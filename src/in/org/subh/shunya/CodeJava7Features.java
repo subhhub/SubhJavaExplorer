86382ed8add22beb8d237c0a3d9d8f041441f759
subhash kewat <subhash.kewat@ericsson.com>
2017-09-08 15:16:37 +0530

    New classes added

---
+++ src/in/org/subh/shunya/CodeJava7Features.java
@@ -0,0 +1,76 @@
+package in.org.subh.shunya;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeJava7Features {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void testStringInSwitch(String param) {
+
+		final String JAVA5 = "Java 5";
+		final String JAVA6 = "Java 6";
+		final String JAVA7 = "Java 7";
+
+		switch (param) {
+		case JAVA5:
+			System.out.println(JAVA5);
+			break;
+		case JAVA6:
+			System.out.println(JAVA6);
+			break;
+		case JAVA7:
+			System.out.println(JAVA7);
+			break;
+		}
+	}
+
+	public void testBinaryIntegralLiterals() {
+
+		int binary = 0b1000; // 2^3 = 8
+
+		if (binary == 8) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+
+	public void testUnderscoresNumericLiterals() {
+
+		int oneMillion_ = 1_000_000; // new
+		int oneMillion = 1000000;
+
+		if (oneMillion_ == oneMillion) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+
+	public void testDinamond() {
+		List<String> list = new ArrayList<>();
+	}
+
+	public void testMultiCatch() {
+
+		try {
+			throw new FileNotFoundException("FileNotFoundException");
+		} catch (ArrayIndexOutOfBoundsException | IOException fnfo) {
+			fnfo.printStackTrace();
+		}
+	}
+
+	public void testTryWithResourcesStatement() throws FileNotFoundException, IOException {
+		
+		try (FileInputStream in = new FileInputStream("java7.txt")) {
+			System.out.println(in.read());
+		}
+	}
+}
---
+++ src/in/org/subh/shunya/CodeJava8LambdaExpression.java
@@ -0,0 +1,87 @@
+package in.org.subh.shunya;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeJava8LambdaExpression {
+
+	public static void main(String[] args) {
+
+//		funcationalInterfaceImplOld();
+		funcationalInterfaceImplLambda();
+
+	}
+
+	static void funcationalInterfaceImplOld() {
+
+		final int width = 10;
+
+		// without lambda, Drawable implementation using anonymous class
+		Drawable d = new Drawable() {
+			public void draw() {
+				System.out.println("Drawing " + width);
+			}
+		};
+
+		d.draw();
+	}
+
+	static void funcationalInterfaceImplLambda() {
+
+		int width = 10;
+
+		// with lambda
+		Drawable d2 = () -> { 
+			System.out.println("with lambda Drawing " + width);
+		};
+		d2.draw();
+		
+//		DrawableArg d3 = (name) -> {
+		DrawableArg d3 = name -> {
+			System.out.println("Argument Lambda Expression "+ name);
+		};
+		d3.draw("subh");
+		
+        // Multiple parameters in lambda expression  
+        Addable ad1=(a,b)->(a+b);  
+        System.out.println(ad1.add(10,20));  
+          
+        // Multiple parameters with data type in lambda expression  
+        Addable ad2=(int a,int b)->(a+b);  
+        System.out.println(ad2.add(100,200));  
+        
+     // Lambda expression with return keyword.    
+        Addable ad3=(int a,int b)->{  
+                            return (a+b);   
+                            };  
+        System.out.println(ad3.add(100,200));  
+        
+        //Lambda Expression Example: Foreach Loop
+        List<String> list=new ArrayList<String>();  
+        list.add("ankit");  
+        list.add("mayank");  
+        list.add("irfan");  
+        list.add("jai");  
+          
+        list.forEach(  
+            (n)->System.out.println(n)  
+        );  
+
+	}
+}
+
+@FunctionalInterface		//Optional if a interface has only one abstract method then it called functional interface
+interface Drawable {
+	public void draw();
+}
+
+
+interface DrawableArg {
+	public void draw(String str);
+}
+
+interface Addable{  
+    int add(int a,int b);  
+}  
+
+
