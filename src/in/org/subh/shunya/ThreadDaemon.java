78e122aa90854dfc46f91c8fbe9594e9349b3c96
subhash kewat <subhash.kewat@ericsson.com>
2017-09-08 12:33:34 +0530

    New Class Thread Daemon

--- src/in/org/subh/shunya/ThreadDaemon.java
+++ src/in/org/subh/shunya/ThreadDaemon.java
@@ -1,7 +1,28 @@
 package in.org.subh.shunya;
 
 public class ThreadDaemon {
+	
 	public static void main(String[] args) {
 		System.out.println("Thread Daemon ");
+
+		Thread daemonThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						System.out.println("Daemon thread is running");
+					}
+
+				} catch (Exception e) {
+
+				} finally {
+					System.out.println("Daemon Thread exiting"); // never called
+				}
+			}
+		}, "Daemon-Thread");
+
+		daemonThread.setDaemon(true); // making this thread daemon
+
+		daemonThread.start();
 	}
 }
